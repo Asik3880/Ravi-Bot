96057776c58967ea378cba39ff0716e9c281f28a
spidy85 <142041169+spidy85@users.noreply.github.com>
2023-09-04 22:23:21 +0530

    hope

--- plugins/commands.py
+++ plugins/commands.py
@@ -374,8 +374,8 @@ async def start(client, message):
         # if temp.SHORT.get(user)==None:
         #     return await message.reply_text(text="<b>Pʟᴇᴀsᴇ Sᴇᴀʀᴄʜ Aɢᴀɪɴ ɪɴ Gʀᴏᴜᴘ</b>")
         # else:
-        #     chat_id = temp.SHORT.get(user)
-        # settings = await get_settings(chat_id)
+        chat_id = temp.SHORT.get(user)
+        settings = await get_settings(chat_id)
         if settings['is_shortlink'] and user not in PREMIUM_USER:
             files_ = await get_file_details(file_id)
             files = files_[0]
