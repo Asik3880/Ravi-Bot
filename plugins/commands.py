50ac859ba18dfb56346fb34cdb82e9b4ce6d827b
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2023-09-24 23:38:24 +0530

    😞

--- plugins/commands.py
+++ plugins/commands.py
@@ -1278,7 +1278,7 @@ async def remove_premium_cmd_handler(client, message):
     else:
         await message.reply_text("Usage: /remove_premium user_id")
         
-@Client.on_message(filters.command("/plans"))
+@Client.on_message(filters.command("plans"))
 async def plans_cmd_handler(client, message):                
     btn = [            
         [InlineKeyboardButton("✅sᴇɴᴅ ʏᴏᴜʀ ᴘᴀʏᴍᴇɴᴛ ʀᴇᴄᴇɪᴘᴛ ʜᴇʀᴇ✅", url="t.me/Mr_SPIDY")],
