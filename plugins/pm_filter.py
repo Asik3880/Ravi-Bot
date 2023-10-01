a7d344c86429fc97d93bef71a220251b6d931854
Asik3880 <141816024+Asik3880@users.noreply.github.com>
2023-10-01 22:46:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -938,7 +938,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             fileName = {quote_plus(get_name(log_msg))}
             page_link = f"{STREAM_URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
             stream_link = f"{STREAM_URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"                
-                buttons = [
+            buttons = [
                     InlineKeyboardButton(
                         "📥 Fast Download",
                         url=stream_link,
