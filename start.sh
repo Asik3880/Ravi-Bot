32a4c1c4c0ef3d2ac662b42eaadf7a36e7246257
Asik3880 <141816024+Asik3880@users.noreply.github.com>
2024-01-04 16:18:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://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@github.com/Asik3880/FM-BOT.git /FM-BOT1
+  git clone https://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@github.com/Asik3880/FM-BOT.git /FM-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FM-BOT1
+  git clone $UPSTREAM_REPO /FM-BOT
 fi
-cd /FM-BOT1
+cd /FM-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...!!!"
 python3 bot.py
