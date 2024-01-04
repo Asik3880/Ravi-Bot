8706628951612f3f57c26d34237212cdab724c73
Asik3880 <141816024+Asik3880@users.noreply.github.com>
2024-01-04 16:24:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://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@github.com/Asik3880/FM-BOT.git /FM-BOT
+  git clone https://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@github.com/Asik3880/FM-BOT.git /FM-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /FM-BOT
